507984fdc2b7eab47a28593fbf684edbf2a11788
Cyrus Buhariwalla <cyrusb01@outlook.com>
2022-04-04 16:39:30 -0500

    grid template

--- app/routes.py
+++ app/routes.py
@@ -3,8 +3,23 @@ from flask import render_template
 
 @app.route('/')
 def index():
+    """
+    This route should be the main page, 
+    grid of all items
+    """
     query = "shop data"
-    return(render_template('index.html', data=query))
+    parent_list = [
+            {'Phone1': ["Item Description", "./static/download.jpg"]}, 
+            {'Phone2': ["Item Description", "./static/download.jpg"] }, 
+            {'Phone3': ["Item Description", "./static/download.jpg"]}, 
+            {'Phone4': ["Item Description", "./static/download.jpg"]}, 
+            {'Phone5': ["Item Description", "./static/download.jpg"]}, 
+            {'Phone6': ["Item Description", "./static/download.jpg"]}, 
+            {'Phone7': ["Item Description", "./static/download.jpg"]}, 
+            {'Phone8': ["Item Description", "./static/download.jpg"]}, 
+            {'Phone9': ["Item Description", "./static/download.jpg"]}, 
+            {'Phone10': ["Item Description", "./static/download.jpg"]}]
+    return(render_template('index.html', data=parent_list))
 
 @app.route('/shop')
 def shop():
