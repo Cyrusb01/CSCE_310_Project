d0f8f74d342dacfd7bf24f7bbe723415640f7b17
michael-norman-7 <mnorman0801@gmail.com>
2022-04-06 14:30:12 -0500

    Added Item Page

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,8 @@
-from app import app 
+from app import app
 from flask import render_template
 
 
-#change
+# change
 @app.route('/')
 def index():
     """
@@ -10,26 +10,42 @@ def index():
     grid of all items
     """
     parent_list = [
-            {'Phone1': ["Item Description", "./static/download.jpg"]}, 
-            {'Phone2': ["Item Description", "./static/download.jpg"] }, 
-            {'Phone3': ["Item Description", "./static/download.jpg"]}, 
-            {'Phone4': ["Item Description", "./static/download.jpg"]}, 
-            {'Phone5': ["Item Description", "./static/download.jpg"]}, 
-            {'Phone6': ["Item Description", "./static/download.jpg"]}, 
-            {'Phone7': ["Item Description", "./static/download.jpg"]}, 
-            {'Phone8': ["Item Description", "./static/download.jpg"]}, 
-            {'Phone9': ["Item Description", "./static/download.jpg"]}, 
-            {'Phone10': ["Item Description", "./static/download.jpg"]}]
+        {'Phone1': ["Item Description", "./static/download.jpg"]},
+        {'Phone2': ["Item Description", "./static/download.jpg"]},
+        {'Phone3': ["Item Description", "./static/download.jpg"]},
+        {'Phone4': ["Item Description", "./static/download.jpg"]},
+        {'Phone5': ["Item Description", "./static/download.jpg"]},
+        {'Phone6': ["Item Description", "./static/download.jpg"]},
+        {'Phone7': ["Item Description", "./static/download.jpg"]},
+        {'Phone8': ["Item Description", "./static/download.jpg"]},
+        {'Phone9': ["Item Description", "./static/download.jpg"]},
+        {'Phone10': ["Item Description", "./static/download.jpg"]}]
     return(render_template('index.html', data=parent_list))
+
 
 @app.route('/shop')
 def shop():
     return(render_template('shop.html'))
 
+
 @app.route('/login', methods=["GET", "POST"])
 def login():
     return(render_template('login.html'))
 
+
 @app.route('/register')
 def register():
     return(render_template('register.html'))
+
+
+@app.route('/item')
+def item():
+    item_name = "Test Item Name"
+    item_price = 310.99
+    item_desc = """Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."""
+    item_pic_path = "./static/download.jpg"
+
+    return(render_template('item.html', **locals()))
